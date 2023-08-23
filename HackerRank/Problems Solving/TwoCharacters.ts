90eb7c26ee340c1e221f1985f19824d04f32ae41
Staroz <marcourquidi17@gmail.com>
2023-08-23 04:30:51 -0400

    08-23-23

---
+++ HackerRank/Problems Solving/TwoCharacters.ts
@@ -0,0 +1,74 @@
+// DATE 08/23/23
+export function alternate(s: string): number {
+
+    let array:string[] = []
+    let brr: string[];
+    let max = 0
+  
+    for (let index = 0; index < s.length; index++) {
+        if (!array.find(x=> x ==s[index])) {
+            array.push(s[index])
+        }       
+    }
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 1; j < array.length; j++) {
+            brr = [...s].filter(x=> x==array[i]|| x==array[j])
+            if (brr.join('').match(/([a-zA-Z])\1/g)== null && brr.length>max) {
+                    max = brr.length;
+            } 
+            
+        }
+        
+    }
+
+    return max;
+}
+
+
+/*
+https://www.hackerrank.com/challenges/two-characters/problem
+DESCRIPTION:
+
+YGiven a string, remove characters until the string is made up of any two alternating characters. When you choose a character to remove, all instances of that character must be removed. Determine the longest string possible that contains just two alternating letters.
+
+Example
+
+
+Delete a, to leave bcdbd. Now, remove the character c to leave the valid string bdbd with a length of 4. Removing either b or d at any point would not result in a valid string. Return .
+
+Given a string , convert it to the longest possible string  made up only of alternating characters. Return the length of string . If no string  can be formed, return .
+
+Function Description
+
+Complete the alternate function in the editor below.
+
+alternate has the following parameter(s):
+
+string s: a string
+Returns.
+
+int: the length of the longest valid string, or  if there are none
+Input Format
+
+The first line contains a single integer that denotes the length of .
+The second line contains string .
+
+Constraints
+
+Sample Input
+
+STDIN       Function
+-----       --------
+10          length of s = 10
+beabeefeab  s = 'beabeefeab'
+Sample Output
+
+5
+Explanation
+
+The characters present in  are a, b, e, and f. This means that  must consist of two of those characters and we must delete two others. Our choices for characters to leave are [a,b], [a,e], [a, f], [b, e], [b, f] and [e, f].
+
+If we delete e and f, the resulting string is babab. This is a valid  as there are only two distinct characters (a and b), and they are alternating within the string.
+
+If we delete a and f, the resulting string is bebeeeb. This is not a valid string  because there are consecutive e's present. Removing them would leave consecutive b's, so this fails to produce a valid string .
+*/
